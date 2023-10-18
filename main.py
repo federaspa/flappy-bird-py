5d25bda49d1592c8803bf7040ad3fa86c7762d13
Federico Raspanti <federico.raspanti2000@hotmail.com>
2023-10-18 16:34:30 +0200

    Changed pipe spawning

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame as pg
+from random import randint
 import sys
 
 pg.init()
@@ -28,18 +29,35 @@ pipe_down_surf = pg.transform.flip(pipe_up_surf, 0,1)
 pipe_up_rect = pipe_up_surf.get_rect(midtop = (300, 350))
 pipe_down_rect = pipe_down_surf.get_rect(midbottom = (300 , 200))
 
-pipe_rects = [pipe_up_rect, pipe_down_rect]
+pipe_rects = []
+
+pipe_timer = pg.USEREVENT +1
+pg.time.set_timer(pipe_timer, 1500)
 
 def reset_game():
-    pipe_up_rect.midtop = (300, 350)
-    pipe_down_rect.midbottom = (300, 200)
-
+    pipe_rects = []
     bird_rect.center = (90, 256)
+    return pipe_rects
+
+
+def spawn_pipes(pipe_rects):
+    new_center = randint(150,300)
+    pipe_up_rect = pipe_up_surf.get_rect(midtop = (350, new_center+75))
+    pipe_down_rect = pipe_down_surf.get_rect(midbottom = (350 , new_center -75))
+    pipe_rects.append([pipe_up_rect, pipe_down_rect])
+    return pipe_rects
+
+
+def display_pipes(pipe_rects):
+    for pipes in pipe_rects:
+        if len(pipes)==2:
+            screen.blit(pipe_up_surf, pipes[0])
+            screen.blit(pipe_down_surf, pipes[1])
+
 
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
-            
             pg.quit()
             exit()
 
@@ -47,20 +65,24 @@ while True:
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_SPACE:
                     gravity = -15
-
+            if event.type == pipe_timer:
+                pipe_rects = spawn_pipes(pipe_rects)
         else:
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_SPACE:
-                    reset_game()
+                    pipe_rects = reset_game()
                 gravity = -15
                 game_active = 1
 
 
     if game_active:
-        for pipe in pipe_rects:
-            pipe.left -= 3
-            if pipe.right < 0:
-                pipe.left = 288
+        for pipes in pipe_rects:
+            for pipe in pipes:
+                pipe.left -= 3
+                if pipe.right < 0:
+                    pipes.remove(pipe)
+            if not pipes:
+                pipe_rects.remove(pipes)
 
 
         gravity += 1
@@ -68,14 +90,14 @@ while True:
         if bird_rect.bottom > 450:
             bird_rect.bottom = 450
 
-        if any(bird_rect.colliderect(pipe) for pipe in pipe_rects):
-            game_active = 0
+        for pipes in pipe_rects:
+            if any(bird_rect.colliderect(pipe) for pipe in pipes):
+                game_active = 0
 
     screen.blit(bg_surf, (0,0))
     screen.blit(text_surf, text_rect)
     screen.blit(bird_surf, bird_rect)
-    screen.blit(pipe_up_surf, pipe_up_rect)
-    screen.blit(pipe_down_surf, pipe_down_rect)
+    display_pipes(pipe_rects)
     screen.blit(floor_surf, (0,450))
 
     pg.display.update()
